9bfde9a9af9604b5bd2099dfc15cd1eee136c8ef
Aditi Admuthe <aditiadmuthe27@gmail.com>
2026-07-18 12:54:15 +0530

    Updated app for Render deployment

--- app.py
+++ app.py
@@ -391,5 +391,7 @@ if __name__ == "__main__":
         db.create_all()
 
     app.run(
-        debug=True
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
     )
